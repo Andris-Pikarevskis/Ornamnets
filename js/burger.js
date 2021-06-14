af544eee14a766a33690abc382e898bb65fe1a64
Andris Pikarevskis <A.Pikarevskis@latinsoft.lv>
2021-06-14 15:35:40 +0300

    croll-on-click

--- js/burger.js
+++ js/burger.js
@@ -29,10 +29,10 @@ const isMobile = {
 if (isMobile.any()) {
     document.body.classList.add('_touch');
     let menuArrows = document.querySelectorAll('.menu__arrow');
-    if (menuArrows.length>0){
-        for(let index = 0; index < menuArrows.length; index++) {
+    if (menuArrows.length > 0) {
+        for (let index = 0; index < menuArrows.length; index++) {
             const menuArrow = menuArrows[index];
-            menuArrow.addEventListener("click", function(e){
+            menuArrow.addEventListener("click", function (e) {
                 menuArrow.parentElement.classList.toggle('_active');
             });
         }
@@ -40,3 +40,27 @@ if (isMobile.any()) {
 } else {
     document.body.classList.add('_pc');
 }
+
+//----------------------------------------------------------roll-on-click---------------------------------------------------------//
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
+
